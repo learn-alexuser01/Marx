941643ed27a19fb3757d4043bcb91cf063f008a5
Douglas Chen <douglaspchen@gmail.com>
2013-09-28 16:36:20 -0700

    fix pages owner/user confusion

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,24 @@
 class PagesController < ApplicationController
   def show
+    @page = Page.find(params[:id])
+
+    respond_to do |format|
+      format.json {render json: @page}
+    end
   end
 
   def create
+    Page.new({owner: current_user})
+
   end
 
   def update
+    @page = Page.find(params[:id])
+
   end
 
   def delete
+    @page = Page.find(params[:id])
+    @page.destroy
   end
 end
