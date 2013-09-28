dc7c08d1468e03d8db5e69f8a055f990067ace99
Douglas Chen <douglaspchen@gmail.com>
2013-09-28 16:13:34 -0700

    add pages controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Marx::Application.routes.draw do
+  resources :pages, only: [:create, :update, :delete]
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => "home#index"
 end
