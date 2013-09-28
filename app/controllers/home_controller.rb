61c76446d0ed51b8b96d3800b3b8dc2e8dc70d19
Douglas Chen <douglaspchen@gmail.com>
2013-09-28 16:29:09 -0700

    add pages to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
     FlickRaw.api_key="18ec419d7ff691b6b202b8012b63ed41"
     FlickRaw.shared_secret="5f06991ff3d25cd2"
     @photos = flickr.interestingness.getList( :per_page => 1, :page => 1 )
+    @pages = Page.where({user_id: current_user}).all
   end
 end
