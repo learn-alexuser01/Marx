ac3ede57d8549aa94483f649213f178b116efa27
Douglas Chen <douglaspchen@gmail.com>
2013-09-28 18:19:15 -0700

    add some debugging for the pages#show method in javascript

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     FlickRaw.api_key="18ec419d7ff691b6b202b8012b63ed41"
     FlickRaw.shared_secret="5f06991ff3d25cd2"
     @interesting_photos = flickr.interestingness.getList( :per_page => 1, :page => 1 )
+
+  def not_found
+      raise ActionController::RoutingError.new('Not Found')
+  end
 end
