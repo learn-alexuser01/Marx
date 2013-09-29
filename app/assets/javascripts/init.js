44c57054ee1265ebe9d419c39e9cc464699b5860
Prem Nair <prem.q.nair@gmail.com>
2013-09-28 19:52:20 -0700

    Demo gridster more

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,7 +2,9 @@ $(function(){ //DOM Ready
  
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
-        widget_base_dimensions: [140, 140]
+        widget_base_dimensions: [140, 140],
+        min_cols:5,
+        min_rows:2
     });
  
 });
