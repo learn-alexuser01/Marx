179c7e97c100aa4183c814f25c550298806ee216
Prem Nair <prem.q.nair@gmail.com>
2013-09-28 20:52:26 -0700

    5 cols only

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -3,8 +3,7 @@ $(function(){ //DOM Ready
     $(".gridster ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [140, 140],
-        min_cols:5,
-        min_rows:2
+        max_cols:5
     });
  
 });
