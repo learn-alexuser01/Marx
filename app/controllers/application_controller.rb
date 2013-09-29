9af924149472bfeb1d343d2077e917b4f13e1820
Douglas Chen <douglaspchen@gmail.com>
2013-09-29 00:27:59 -0700

    add some image variety

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def interesting_photo
-    return flickr.interestingness.getList( :per_page => 1, :page => 1 )
+    return flickr.interestingness.getList( :per_page => 1, :page => 10 )
   end
 
   def not_found
