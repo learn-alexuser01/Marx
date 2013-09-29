072ff515384076fdc18a53320ad7303c2ec2b234
Douglas Chen <douglaspchen@gmail.com>
2013-09-28 18:09:04 -0700

    add devise sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Marx::Application.routes.draw do
   resources :pages, only: [:create, :update, :delete, :show]
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", sessions: "users/sessions" }
   root :to => "home#index"
 end
