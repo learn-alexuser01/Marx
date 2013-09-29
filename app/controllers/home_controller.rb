ebcf207f0fb655efc2d1ad3001004d581d3f7076
Shrav Mehta <shravvmehtaa@gmail.com>
2013-09-29 12:22:00 -0700

    fixed search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @photos = flickr.interestingness.getList( :per_page => 10, :page => 1 )
-    @rel_photo = flickr.photos.search(:text => "test", :per_page =>'10', :page => 1, :sort => 'relevance', :safe_search => '1')
+    @photos = flickr.photos.search(:text => 'landscapes', :per_page =>'10', :page => 1, :sort => 'relevance', :safe_search => '1')
     @pages = Page.where({owner: current_user}).all
   end
 end
