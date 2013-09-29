5bf09bb1adaf35fd914451676dbcd4627a14be7e
Shrav Mehta <shravvmehtaa@gmail.com>
2013-09-28 21:14:24 -0700

    finished login image

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
+  def new
+    @photos = interesting_photo
+    super
+  end
 end
---
+++ app/controllers/users/sign_in_controller.rb
@@ -0,0 +1,2 @@
+class Users::SignInController < Devise::RegistrationsController
+end
